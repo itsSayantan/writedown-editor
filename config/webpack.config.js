d8348d8d121e7b261a8d21303e20a500ce6f49a1
iamparnab <sanyal.parnab96@gmail.com>
2019-11-19 23:32:52 +0530

    1. Basic Project setup for writedown editor(the heart)

---
+++ config/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const webpack = require("webpack");
+
+const REACT_BASE_PATH = "../";
+console.log(path.resolve(__dirname, REACT_BASE_PATH));
+module.exports = {
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  devtool:
+    process.env.NODE_ENV === "production"
+      ? "source-map"
+      : "cheap-module-eval-source-map",
+  entry: "./index.tsx",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    alias: {
+      "@Components": path.resolve(__dirname, REACT_BASE_PATH + "components/"),
+      "@Shared": path.resolve(__dirname, REACT_BASE_PATH + "shared/")
+    }
+  },
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, REACT_BASE_PATH),
+    disableHostCheck: true,
+    port: 8080,
+    compress: true,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /.tsx{0,1}$/,
+        use: ["ts-loader"]
+      }
+    ]
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
